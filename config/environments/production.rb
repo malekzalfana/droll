e09142f40377dcf1c9f900b860b4a61356ea22ec
Malek <zalfanamalek@gmail.com>
2016-09-22 21:32:39 +0000

    fixed images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
