d5ea2e349bab35f521446532d591318a8ab27c0e
Malek <zalfanamalek@gmail.com>
2017-05-08 10:54:28 +0000

    trend fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -89,7 +89,7 @@ class PagesController < ApplicationController
 
         @trendname = post.trend_list
         if post.trendid.blank?
-          if Trend.where(name: @trendname)[0].exists?
+          if Trend.where(name: @trendname)[0]
             post.trendid = Trend.where(name: @trendname)[0].id
             post.save
           else
